68e5b371137dc902c1aea585b12c0bdd941518e5
Ahmad Ahdani Dzikron <80938624+ahdani123@users.noreply.github.com>
2025-11-28 13:51:52 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// URL Google Sheets CSV
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-Iw7Ou8GyhlBVW_aVhj6xPlncGhtMgYBzzHxnCXMkY5-pNp0lAzEXgov7SM8MlrvaKASy0-AQzQk4/pub?output=csv";
+
+async function loadData() {
+    const response = await fetch(sheetURL);
+    const csv = await response.text();
+
+    const rows = csv.trim().split("\n").map(r => r.split(","));
+    const header = rows.shift().map(h => h.trim());
+
+    const data = rows.map(row => {
+        let obj = {};
+        header.forEach((h, i) => obj[h] = row[i] ? row[i].trim() : "");
+        return obj;
+    });
+
+    displayProducts(data);
+}
+
+function displayProducts(products) {
+    const container = document.getElementById("product-list");
+    container.innerHTML = "";
+
+    products.forEach(item => {
+        const card = document.createElement("div");
+        card.className = "product-card";
+
+        card.innerHTML = `
+            <img src="${item.gambar}" class="product-image" alt="${item.nama}">
+            <h3>${item.nama}</h3>
+            <p>Jenis: ${item.jenis}</p>
+            <p>Harga: Rp ${item.harga}</p>
+            <p>Stok: ${item.stok}</p>
+        `;
+
+        container.appendChild(card);
+    });
+}
+
+loadData();
